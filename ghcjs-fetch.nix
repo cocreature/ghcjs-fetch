0994dcb3b70f9d6f175bd8e370b00b094c75de77
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-09-25 20:28:57 +0200

    Regenerate nix config

--- ghcjs-fetch.nix
+++ ghcjs-fetch.nix
@@ -1,18 +1,19 @@
-{ mkDerivation, aeson, base, bytestring, ghcjs-base, hspec
-, hspec-core, http-types, QuickCheck, stdenv, text
-, unordered-containers
+{ mkDerivation, aeson, base, bytestring, case-insensitive
+, ghcjs-base, hspec, hspec-core, http-types, QuickCheck, stdenv
+, text, unordered-containers
 }:
 mkDerivation {
   pname = "ghcjs-fetch";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base bytestring ghcjs-base http-types
+    aeson base bytestring case-insensitive ghcjs-base http-types
   ];
   testHaskellDepends = [
     aeson base ghcjs-base hspec hspec-core http-types QuickCheck text
     unordered-containers
   ];
   homepage = "https://github.com/cocreature/ghcjs-fetch#readme";
+  description = "GHCJS bindings for the JavaScript Fetch API";
   license = stdenv.lib.licenses.bsd3;
 }
